d0f305b8a877237ce3fb9995071b959f4bc657dd
Diki SyCops - 发作 <dofthgumyxd@gmail.com>
2022-09-16 04:10:59 +0700

    kkkakkakakakako

--- hwkal.js
+++ hwkal.js
@@ -19,19 +19,19 @@ global.mygc = 'wa.me/6285714170944'
 global.myweb = 'https://eclass.iainsalatiga.ac.id/app/upload/users/1/10892/my_files/XhiroMhonshine.html'
 global.linkgrupss = "https://chat.whatsapp.com/Jh7zaCSnPCBC4gdYOEVm0K"
 global.linkpro = 'mailto:support@whatsapp.com?subject=Deactivate%20My%20Account%20Number&body=Please%20Deactivate%20The%20My%20Account%20Number%20%2B62XXXXXXXX%20Immediately%20Because%20The%20Number%20Has%20Been%20Lost'
-global.email = 'denyp857@gmail.com'
+global.email = 'kenz.hm.421@gmail.com'
 global.region = 'indonesia'
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'HW MODS WA' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'D2V' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'D2V' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'D2V' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
-global.owner = ['6281214281312']
-global.premium = ['6281214281312']
-global.packname = 'HW MODS WA'
-global.ownerr = ['HW MODS WA']
-global.author = 'HW MODS WA'
+global.owner = ['6283162498175','6288218525085']
+global.premium = ['6283162498175']
+global.packname = 'D2V Chan'
+global.ownerr = ['66XZD']
+global.author = 'D2V'
 global.sessionName = './sessionye/haikal'
 global.prefa = ['','!','.',',','🐤','🗿']
 global.sp = '⭔'
